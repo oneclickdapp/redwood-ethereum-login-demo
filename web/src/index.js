68edc2e4ec280001cbc92e46f0ca25c87e2a324e
Patrick Gallagher <Blockchainbuddha@gmail.com>
2020-12-29 17:55:40 -0500

    generate auth command

---
+++ web/src/index.js
@@ -0,0 +1,23 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import EthereumAuthClient from '@oneclickdapp/ethereum-auth'
+import ReactDOM from 'react-dom'
+import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+
+import Routes from 'src/Routes'
+
+import './index.css'
+
+const graphQLClient = createGraphQLClient()
+const ethereum = new EthereumAuthClient({ graphQLClient })
+
+ReactDOM.render(
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <AuthProvider client={ethereum} type="ethereum">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
+  </FatalErrorBoundary>,
+  document.getElementById('redwood-app')
+)
